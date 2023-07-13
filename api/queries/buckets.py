44db2fd9614f709b0b676ec09cce7a012c626499
Elena <elenailieva@yahoo.com>
2023-07-13 16:08:10 -0500

    commit x

--- api/queries/buckets.py
+++ api/queries/buckets.py
@@ -1,7 +1,7 @@
 import requests
 import os
 from models.buckets import BucketIn, BucketOut
-from models.films import FilmData
+from models.films import FilmData, Films, FilmOut
 from queries.pool import pool
 from typing import Optional, List
 
@@ -31,6 +31,86 @@ class BucketsQueries:
                 ]
                 return buckets
 
+
+    def list_films_in_buckets(self, bucket_id: str) -> Optional[List[Films]]:
+        with pool.connection() as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(
+                    """
+                    SELECT films.id, films.title
+                    FROM buckets_films
+                    INNER JOIN films ON films.id = buckets_films.film_id
+                    WHERE bucket_id = %s;
+                    """,
+                    (bucket_id,),
+                )
+                rows = cursor.fetchall()
+                films_in_buckets = [
+                    FilmOut(
+                        id=row[0],
+                        title=row[1],
+                    )
+                    for row in rows
+                ]
+                return Films(films=films_in_buckets)
+
+    def add_film_to_bucket(
+        self, bucket_id: str, film_id: int
+    ) -> Optional[FilmData]:
+        url = f"https://api.themoviedb.org/3/movie/{film_id}?api_key={TMDB_API_KEY}"
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            film_data = response.json()
+            if "id" in film_data:
+                with pool.connection() as conn:
+                    with conn.cursor() as cursor:
+                        # Update films table
+                        cursor.execute(
+                            """
+                            INSERT INTO films (id, title)
+                            VALUES (%s, %s)
+                            ON CONFLICT (id) DO UPDATE SET title = EXCLUDED.title;
+                            """,
+                            (film_data["id"], film_data["title"]),
+                        )
+
+                        # Insert into buckets_films table
+                        cursor.execute(
+                            """
+                            INSERT INTO buckets_films (bucket_id, film_id)
+                            VALUES (%s, %s);
+                            """,
+                            (bucket_id, film_data["id"]),
+                        )
+                        conn.commit()
+
+                        film_data = FilmData(
+                            bucket_id=bucket_id,
+                            film_data=film_data,
+                            success=True,
+                        )
+                        return film_data
+
+    def delete_bucket(self, bucket_id: int) -> bool:
+        try:
+            # connect the database
+            with pool.connection() as conn:
+                # get a cursor
+                with conn.cursor() as db:
+                    db.execute(
+                        """
+                            DELETE FROM buckets
+                            WHERE id = %s
+                            """,
+                        [bucket_id],
+                    )
+
+                    return True
+        except Exception as e:
+            print(e)
+            return False
+
     def create_bucket(self, bucket: BucketIn) -> Optional[BucketOut]:
         with pool.connection() as conn:
             with conn.cursor() as cursor:
@@ -49,32 +129,6 @@ class BucketsQueries:
                     account_id=bucket.account_id,
                 )
                 return new_bucket
-
-    def add_film_to_bucket(
-        self, bucket_id: str, film_id: int
-    ) -> Optional[FilmData]:
-        url = f"https://api.themoviedb.org/3/movie/{film_id}?api_key={TMDB_API_KEY}"
-        response = requests.get(url)
-
-        if response.status_code == 200:
-            film_data = response.json()
-            if "id" in film_data:
-                with pool.connection() as conn:
-                    with conn.cursor() as cursor:
-                        cursor.execute(
-                            """
-                            INSERT INTO buckets_films (bucket_id, film_id)
-                            VALUES (%s, %s);
-                            """,
-                            (bucket_id, film_data["id"]),
-                        )
-                        conn.commit()
-                        film_data = FilmData(
-                            bucket_id=bucket_id,
-                            film_data=film_data,
-                            success=True,
-                        )
-                        return film_data
 
     def update_bucket_name(
         self, bucket_id: str, updated_name: str
--- api/routers/buckets.py
+++ api/routers/buckets.py
@@ -42,7 +42,34 @@ def update_bucket_name(
     account_data: dict = Depends(authenticator.get_current_account_data),
     queries: BucketsQueries = Depends(),
 ):
+    if account_data is None:
+        raise HTTPException(status_code=401, detail="Not logged in")
+    elif not queries.get_buckets_by_user(bucket_id):
+        raise HTTPException(status_code=404, detail="Bucket not found")
+    elif account_data["id"] != queries.get_buckets_by_user(bucket_id).account_id:
+        raise HTTPException(status_code=401, detail="Not authorized to modify this bucket")
     return queries.update_bucket_name(
         bucket_id=bucket_id, updated_name=updated_bucket.name
     )
 
+@router.delete("/buckets/{bucket_id}", response_model=bool)
+def delete_bucket(bucket_id: str,
+    account_data: dict = Depends(authenticator.get_current_account_data),
+    queries: BucketsQueries = Depends(),
+    ):
+    if account_data is None:
+        raise HTTPException(status_code=401, detail="Not logged in")
+    elif not queries.get_buckets_by_user(bucket_id):
+        raise HTTPException(status_code=404, detail="Bucket not found")
+    elif account_data["id"] != queries.get_buckets_by_user(bucket_id).account_id:
+        raise HTTPException(status_code=401, detail="Not authorized to delete this bucket")
+    buckets = queries.delete_bucket(bucket_id)
+    return buckets
+
+@router.get("/buckets/{bucket_id}/films", response_model=Films)
+def list_films_in_buckets(
+    bucket_id: str,
+    account_data: dict = Depends(authenticator.get_current_account_data),
+    queries: BucketsQueries = Depends(),
+):
+    return queries.list_films_in_buckets(bucket_id=bucket_id)
